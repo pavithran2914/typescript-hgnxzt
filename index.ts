a45f907742edc1e9a22fb3426bd88c9b03260cad
pavithran2914 <pavithranbala29@gmail.com>
2022-02-21 11:35:24 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -8,3 +8,17 @@ import { Line } from './line.model';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
+let point1 = new Dot(2, 2);
+    let point2 = new Dot(8, 2);
+    let point3 = new Dot(2, 4);
+    let point4 = new Dot(8, 4);
+
+
+    let line1 = new Line(point1, point2);
+let line2 = new Line(point2, point4);
+let line3 = new Line(point4, point3);
+let line4 = new Line(point3, point1);
+
+let rectangularObj = new Rectangular(line1, line2, line3, line4);
+
+console.log(rectangularObj);
--- rectangular.model.ts
+++ rectangular.model.ts
@@ -2,7 +2,17 @@ import { Dot } from './dot.model';
 import { Line } from './line.model';
 
 export class Rectangular {
+  line1: Line;
+  line2: Line;
+  line3: Line;
+  line4: Line;
 
+  constructor(line1: Line, line2: Line, line3: Line, line4: Line) {
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+    this.line4 = line4;
+  }
   dot() {
     let obj1 = new Dot(2, 2);
     let obj2 = new Dot(8, 2);
